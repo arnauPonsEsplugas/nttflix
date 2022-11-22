7155c2891139e9cb3e9e1e261bb31320e45a5a03
Alejandro Olid <alejandro.olid.dev@gmail.com>
2022-11-22 11:17:30 +0100

    test

---
+++ clients/clients/src/main/java/com/nttfix/users/clients/models/UserData.java
@@ -0,0 +1,88 @@
+package com.nttfix.users.clients;
+
+@Entity
+@Table
+@Repository
+public class Userdata {
+    Long id;
+
+    String username;
+
+    String password;
+
+    String email;
+
+    String account;
+
+    Boolean exprired;
+
+    public Userdata() {}
+
+    public Userdata(Long id, String username, String password, String email, String account, Boolean exprired) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.account = account;
+        this.exprired = exprired;
+    }
+
+    public String toString() {
+        return "Userdata{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                ", account='" + account + '\'' +
+                ", exprired=" + exprired +
+                '}';
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public Boolean getExprired() {
+        return exprired;
+    }
+
+    public void setExprired(Boolean exprired) {
+        this.exprired = exprired;
+    }
+}
