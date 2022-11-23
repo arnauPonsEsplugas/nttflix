5c3de6029ffdfd82a0fac97ab00ec2c79fd4556c
USERSAD\aponsesp <arnau.ponsesplugas.st@emeal.nttdata.com>
2022-11-23 09:32:31 +0100

    no message

--- clients/clients/src/main/java/com/nttfix/users/clients/controller/UserController.java
+++ clients/clients/src/main/java/com/nttfix/users/clients/controller/UserController.java
@@ -1,10 +1,13 @@
 package com.nttfix.users.clients.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nttfix.users.clients.models.UserData;
+import com.nttfix.users.clients.reposiory.UserRepository;
 import com.nttfix.users.clients.service.UserService;
 
 @RestController
@@ -27,12 +31,18 @@ public class UserController {
         return new ResponseEntity<>(newUser, HttpStatus.OK);
     }
 	
-	@GetMapping(value = "/get/{nombre}")
 	
-	@GetMapping("/objeto/{nombre}")
-    public ResponseEntity<Objeto> getObjeto(@PathVariable("nombre") String nombre) {
-        Objeto objeto = new Objeto("Cuadro", "alta");
+	
+	@GetMapping(value = "/get/{username}")
+	    public ResponseEntity<UserData> getUser(@PathVariable("username") String username) {
+        UserData objeto = userService.readUser(username);
         return new ResponseEntity<> (objeto, HttpStatus.OK);
+    }
+	
+    public Optional<UserData> getUser(Long id){
+        if(!userService.existsById(id))
+            throw new RuntimeException("Game not found.");
+        return userService.findById(id);
     }
 	
 	public ResponseEntity<UserData> getUser(@RequestBody RequestUser user);
